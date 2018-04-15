4e04be2aa3d6e3e9bfb7e284febb46f2ecee3dc4
KATO Kanryu <k.kanryu@gmail.com>
2018-04-15 19:17:07 +0900

    first release

---
+++ ViewModels/PageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaliburnApp1.ViewModels
+{
+    public class BodyViewModel
+    {
+        public bool BodyEnable { get; set; } = true;
+    }
+    public class PageViewModel : Screen
+    {
+        public bool DayEnable { get; set; } = true;
+        public ObservableCollection<BodyViewModel> BodiesModel { get; set; }
+
+        private readonly INavigationService navigationService;
+        public PageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+
+            BodiesModel = new ObservableCollection<BodyViewModel>() {
+                new BodyViewModel()
+            };
+
+            NotifyOfPropertyChange(() => DayEnable);
+            NotifyOfPropertyChange(() => BodiesModel);
+
+        }
+    }
+}
---
+++ Views/PageView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace CaliburnApp1.Views
+{
+    public sealed partial class PageView
+    {
+        public PageView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
